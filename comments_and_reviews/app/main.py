e47900f834e0ac30bb5b268de21fbfc4b4865184
Kirill Shipilov <shipilovk04@gmail.com>
2025-05-09 21:07:29 +0300

    add main.py but cant connect to kafka and mongo

--- comments_and_reviews/app/main.py
+++ comments_and_reviews/app/main.py
@@ -1,2 +1,16 @@
-if __name__ == "__main__":
-    print("running...")
+from app.db import init_mongo
+from app.events.router import Router
+from app.dependencies.services_factory import get_services_factory
+from app.kafka import get_kafka
+
+
+services = get_services_factory()
+router = Router(services)
+
+
+async def main():
+    await init_mongo()
+    consumer, producer = get_kafka()
+    async for request in consumer.events():
+        response = await router.route(request)
+        producer.produce(response)
