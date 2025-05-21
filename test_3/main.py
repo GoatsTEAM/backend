b93d84502266cc380a1bfdb725faa106edc30e99
Павел Мудров <stiks@MacBook-Pro-Pavel.local>
2025-05-22 00:07:50 +0300

    add docker compose

---
+++ test_3/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+
+app = FastAPI(root_path="/test_3", title="Test API")
+
+Instrumentator().instrument(app).expose(app)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, world!"}
+
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/tester")
+def read_test():
+    return {"message": "Hello, Tiny!"}
+
+
+
